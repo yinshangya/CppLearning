e4ea3a8f0fd986fcd3d9252556416781c29a675a
yinshangya <535653614@qq.com>
2026-08-17 16:22:11 +0800

    初始化 C++ 学习仓库：笔记 + 练习代码

---
+++ exercises/test1/test1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "hello world from test1!" << endl;
+    cout << "你好" << endl;
+    return 0;
+} 
---
+++ projects/Project1/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello from VS Code and MSVC!" << endl;
+    return 0;
+}
