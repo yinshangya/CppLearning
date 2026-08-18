0daa83a5ed9d381b6f2f81146594c04fde2d9483
yinshangya <535653614@qq.com>
2026-08-18 23:16:59 +0800

    添加第一章节问题4的代码

---
+++ exercises/chapter1/test4/test4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "You are in months is " << age * 12 << "." << endl;
+    return 0;
+}
